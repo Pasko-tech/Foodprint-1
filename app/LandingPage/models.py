466799997b326e27865eaae4dc4b2443fa85a5db
Pasko-tech <paskochada@gmail.com>
2024-09-07 11:15:10 +0200

    'LandingZone'

--- app/LandingPage/models.py
+++ app/LandingPage/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class LandingPage(models.Model):
+    scan_qr = models.CharField(max_length=255)
+    search_bar = models.CharField(max_length=255)
+    filtering_options = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.search_bar
